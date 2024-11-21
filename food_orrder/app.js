57a2d91f7d79444819510ffee72fed7352c7d6d6
nhi <nhi.tran220915@gmail.edu.vn>
2024-11-21 17:27:17 +0700

    upd

---
+++ food_orrder/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./configs/database");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+connectDB();
+
+
+app.listen(5000, ()=>{
+  console.log("Server run at port 5000");
+})
