52376aa0a6ac6259be27219ccd5ac9eb4fcce59c
nhi <nhi.tran220915@gmail.edu.vn>
2024-11-26 20:17:58 +0700

    upd

--- food_orrder/controllers/category.controller.js
+++ food_orrder/controllers/category.controller.js
@@ -1,24 +1,59 @@
-const categoryModel = require("../models/category.model")
-// CRUD
-module.export = {
+const categoryModel = require("../models/category.model");
+
+const categoryController = {
+    getData: async (req, res) => {
+        try {
+            // Gọi API sử dụng fetch
+            const response = await fetch('http://localhost:3000/api/categories');
+            const data = await response.json();
+            
+            // Truyền dữ liệu vào view EJS
+            res.render('index', { data });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Error fetching data');
+        }
+    },
+
     createCategory: async (req, res) => {
         const body = req.body;
-        const newCategory = await categoryModel.create(body);
-        return res.status(201).json(newCategory);
+        try {
+            const newCategory = await categoryModel.create(body);
+            res.status(201).json(newCategory);
+        } catch (error) {
+            res.status(500).send('Error creating category');
+        }
     },
+
     getCategories: async (req, res) => {
-        const categories = await categoryModel.find();
-        return res.status(200).json(updatedCategory);
+        try {
+            const categories = await categoryModel.find();
+            res.status(200).json(categories);
+        } catch (error) {
+            res.status(500).send('Error fetching categories');
+        }
     },
-    updatedCategory: async (req, res) => {
+
+    updateCategory: async (req, res) => {
         const id = req.params.id;
         const body = req.body;
-        const updatedCategory = await categoryModel.findByIdAndUpdate(id, body, {new: true});
-        return res.status(200).json(updatedCategory);
+        try {
+            const updatedCategory = await categoryModel.findByIdAndUpdate(id, body, { new: true });
+            res.status(200).json(updatedCategory);
+        } catch (error) {
+            res.status(500).send('Error updating category');
+        }
     },
-    deleteCategory: async(req, res) => {
+
+    deleteCategory: async (req, res) => {
         const id = req.params.id;
-        const deletedCategory = await categoryModel.findByIdAndDelete(id);
-        return res.status(200).json(deletedCategory);
+        try {
+            const deletedCategory = await categoryModel.findByIdAndDelete(id);
+            res.status(200).json(deletedCategory);
+        } catch (error) {
+            res.status(500).send('Error deleting category');
+        }
     }
-}
+};
+
+module.exports = categoryController;
--- food_orrder/routers/index.js
+++ food_orrder/routers/index.js
@@ -1,4 +1,5 @@
 const categoryRouter = require("./category.router");
+const foodRouter = require("./food.router");
 
 module.exports = (app) => {
     app.use("/api/categories", categoryRouter);
